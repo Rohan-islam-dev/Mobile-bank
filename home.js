3bb4865383e406af9ac6b446ca0a68368f7def55
Rohan-islam-dev <mdrohanislam253@gmail.com>
2025-08-27 09:59:37 +0600

    cashout toggle

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 const validPin = 1234;
-
+//add money feature
 document.getElementById('add-money-btn').addEventListener('click',function (e) {
     e.preventDefault();
     const bank = document.getElementById('bank').value;
@@ -25,3 +25,32 @@ document.getElementById('add-money-btn').addEventListener('click',function (e) {
     
     document.getElementById('available-balance').innerText = totalNewAvailabeBalance;
 })
+
+//cashout money feature
+document.getElementById('withdraw-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    
+    const amount = parseInt(document.getElementById('widthdraw-amount').value);
+    
+    const avaiableBalance = parseInt(document.getElementById('available-balance').innerText);
+    
+    const totalNewAvailabeBalance = avaiableBalance - amount;
+    
+    document.getElementById('available-balance').innerText = totalNewAvailabeBalance ;
+    
+})
+
+
+
+// toggling feature
+
+document.getElementById('add-button').addEventListener('click',function(){
+    document.getElementById('cash-out-parent').style.display = 'none';
+    document.getElementById('add-money-parent').style.display = 'block';
+})
+
+document.getElementById('cash-out-button').addEventListener('click',function(){
+    document.getElementById('cash-out-parent').style.display = 'block';
+    document.getElementById('add-money-parent').style.display = 'none';
+})
+
