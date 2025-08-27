5b5b0d32f8d301ba46db690f54e44c53663a40ff
Rohan-islam-dev <mdrohanislam253@gmail.com>
2025-08-27 07:51:00 +0600

    validation

---
+++ home.js
@@ -0,0 +1,27 @@
+const validPin = 1234;
+
+document.getElementById('add-money-btn').addEventListener('click',function (e) {
+    e.preventDefault();
+    const bank = document.getElementById('bank').value;
+    const accountNumber = document.getElementById('account-number').value;
+    const amount = parseInt(document.getElementById('add-amount').value);
+
+    const pin = parseInt(document.getElementById('add-pin').value);
+    
+    const avaiableBalance = parseInt(document.getElementById('available-balance').innerText);
+    // console.log(avaiableBalance);
+
+    if(accountNumber.length < 11){
+        alert('Please provide valid accoutn number');
+        return;
+    }
+
+    if(pin !== validPin){
+        alert('Please provide valid pin number')
+        return;
+    }
+
+    const totalNewAvailabeBalance = amount + avaiableBalance;
+    
+    document.getElementById('available-balance').innerText = totalNewAvailabeBalance;
+})
